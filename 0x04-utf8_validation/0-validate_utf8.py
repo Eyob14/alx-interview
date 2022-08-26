1b6d31594b08933a72ab72dd89b15f366b48a0b9
Eyob14 <eyobzebene3580@gmail.com>
2022-08-26 22:15:09 +0300

    0-validate_utf8.py fixed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,43 +3,18 @@
 
 
 def validUTF8(data):
-    """Validates UTF-8 encoding"""
-    index = 0
-    length = len(data)
-    def getBinary(x): return format(x, 'b').zfill(8)
-    while index < length:
-        num = getBinary(data[index])
-        if num[0] == '0':
-            index += 1
-        elif num[0:3] == '110':
-            if length - index > 1:
-                temp = getBinary(data[index+1])
-                if temp[0:2] != '10':
-                    return False
-                else:
-                    index += 2
-            else:
+    """Validates UTF 8"""
+    b = 0
+    for i, n in enumerate(data):
+        byte = n & 0xFF
+        if b:
+            if byte >> 6 != 2:
                 return False
-        elif num[0:4] == '1110':
-            if length - index < 2:
-                return False
-            else:
-                index += 1
-                for i in range(2):
-                    temp = getBinary(data[index])
-                    if temp[0:2] != '10':
-                        return False
-                    index += 1
-        elif num[0:5] == '11110':
-            if length - index > 3:
-                index += 1
-                for i in range(3):
-                    temp = getBinary(data[index])
-                    if temp[0:2] != '10':
-                        return False
-                    index += 1
-            else:
-                return False
-        else:
+            b -= 1
+            continue
+        while (1 << abs(7 - b)) & byte:
+            b += 1
+        if b == 1 or b > 4:
             return False
-    return True
+        b = max(b - 1, 0)
+    return b == 0
