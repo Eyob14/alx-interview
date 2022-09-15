6883a023d4b85c318bfff42d480a0ef997f3033c
Eyob14 <eyobzebene3580@gmail.com>
2022-09-15 22:48:30 +0300

    0-rotate_2d_matrix updated

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-"""Rotates 2-D matrix:"""
+""" -*- coding: rotate matrix """
 
 
-def rotate_2d_matrix(m):
-    """Rotates in-place"""
-    n = len(m)
-    temp1, temp2 = 0, 0
-
-    for j in range(0, len(m) // 2 + 1):
-        for i in range(j, n - 1):
-            # For r in first row, put in same position in col from back
-            temp1 = m[i][n - 1]
-            m[i][n - 1] = m[j][i]
-            # put that temp1 in reverse position in row from bottom
-            temp2 = m[n - 1][n - 1 - i + j]
-            m[n - 1][n - 1 - i + j] = temp1
-            # put that temp2 in same position in col from front
-            temp1 = m[n - 1 - i + j][j]
-            m[n - 1 - i + j][j] = temp2
-            # put that temp1 in reverse position in row from top
-            m[j][i] = temp1
-        n -= 1
+def rotate_2d_matrix(matrix) -> None:
+    """ rotate matrix - transpose """
+    N = len(matrix)
+    for row in range(N):
+        new = []
+        for col in range(N - 1, -1, -1):
+            new.append(matrix[col][row])
+        matrix.append(new)
+    [matrix.pop(0) for i in range(N)]
