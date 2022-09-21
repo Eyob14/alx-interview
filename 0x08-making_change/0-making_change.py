bd24747dfd586713b2bf7b7ad98ef73580947fde
Eyob14 <eyobzebene3580@gmail.com>
2022-09-21 22:16:36 +0300

    0-making_change.py updated again

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-""" This file contains the makechange function """
+"""
+Change comes from within
+"""
 
 
 def makeChange(coins, total):
     """
-    This function computes the coin change problem
+    determine the fewest number of coins****
+    needed to meet a given amount
+    @coins: is a list of the values of the coins
+            in the possession
+    @total: given amount
+    Return:
+            fewest number of coins needed to meet total
+            *** If total is 0 or less, return 0
+            *** If total cannot be met by any number
+                of coins you have, return -1
     """
-
+    if (type(total) is not int or type(coins) is not list):
+        return -1
     if total <= 0:
         return 0
+    try:
+        Min = [float('inf') for i in range(total+1)]
+        Min[0] = 0
+        for i in range(1, total+1):
+            for j in range(len(coins)):
+                if Min[i - coins[j]] + 1 < Min[i]:
+                    Min[i] = Min[i - coins[j]] + 1
 
-    coins.sort(reverse=True)
-    sum = 0
-    i = 0
-    counter = 0
-    num_coins = len(coins)
-    while sum < total and i < num_coins:
-        while coins[i] <= total - sum:
-            sum += coins[i]
-            counter += 1
-            if sum == total:
-                return counter
-        i += 1
-    return -1
+        if Min[total] != float('inf'):
+            return Min[total]
+        else:
+            return -1
+    except Exception:
+        return -1
