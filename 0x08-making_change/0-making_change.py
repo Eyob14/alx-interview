5a000730c1a3864887cc0c12817926e6b3de0751
Eyob14 <eyobzebene3580@gmail.com>
2022-09-21 22:11:33 +0300

    0-making_change.py fixed

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,37 +1,25 @@
 #!/usr/bin/python3
-"""
-This file contains a function that calculates make coin
-change.
-"""
-
-
-from math import inf
+""" This file contains the makechange function """
 
 
 def makeChange(coins, total):
-    """ find the min coin change """
-    storage = {}
+    """
+    This function computes the coin change problem
+    """
 
-    def helper(coins, rem):
-        """ a helper function to find the min coin change"""
-        nonlocal storage
-        if rem in storage:
-            return storage[rem]
-        if rem < 0:
-            return -1
-        if rem == 0:
-            return 0
-
-        minimum = inf
-        for i in coins:
-            if rem-i < -2:
-                continue
-            res = helper(coins, rem-i)
-            if (res >= 0 and res < minimum):
-                minimum = 1 + res
-        storage[rem] = minimum if minimum != inf else -1
-        return storage[rem]
-
-    if total < 1:
+    if total <= 0:
         return 0
-    return helper(coins, total)
+
+    coins.sort(reverse=True)
+    sum = 0
+    i = 0
+    counter = 0
+    num_coins = len(coins)
+    while sum < total and i < num_coins:
+        while coins[i] <= total - sum:
+            sum += coins[i]
+            counter += 1
+            if sum == total:
+                return counter
+        i += 1
+    return -1
