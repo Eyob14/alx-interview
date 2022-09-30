d35fb00c0dfb0351385144afdb95be8671e295ad
Eyob14 <eyobzebene3580@gmail.com>
2022-09-30 23:23:15 +0300

    0-island_perimeter.py updated

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,35 +5,36 @@
     """
 
 
+def dfs(i, j, grid, visited):
+    """
+    This function runs dfs on the grid
+    """
+    if (
+        i < 0 or j < 0 or
+        i >= len(grid) or
+        j >= len(grid[0]) or
+        grid[i][j] == 0
+    ):
+        return 1
+    if (i, j) in visited:
+        return 0
+
+    visited.add((i, j))
+    perimeter = dfs(i, j+1, grid, visited)
+    perimeter += dfs(i+1, j, grid, visited)
+    perimeter += dfs(i, j-1, grid, visited)
+    perimeter += dfs(i-1, j, grid, visited)
+
+    return perimeter
+
+
 def island_perimeter(grid):
     """
     This function calculates the perimeter of the island in the given grid.
     """
     visited = set()
 
-    def dfs(i, j):
-        """
-        This function runs dfs on the grid
-        """
-        if (
-            i < 0 or j < 0 or
-            i >= len(grid) or
-            j >= len(grid[0]) or
-            grid[i][j] == 0
-        ):
-            return 1
-        if (i, j) in visited:
-            return 0
-
-        visited.add((i, j))
-        perimeter = dfs(i, j+1)
-        perimeter += dfs(i+1, j)
-        perimeter += dfs(i, j-1)
-        perimeter += dfs(i-1, j)
-
-        return perimeter
-
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                return dfs(i, j)
+                return dfs(i, j, grid, visited)
